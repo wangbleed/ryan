4645238135d92146049be886f20dfe975040e832
wangbleed <wangbleed@gmail.com>
2015-01-22 09:17:28 +0800

    no message

---
+++ RyanProj/Demo/src/main/java/com/ryan/test/TestClone.java
@@ -0,0 +1,15 @@
+package com.ryan.test;
+
+
+import org.apache.commons.lang3.SerializationUtils;
+
+/**
+ * Created by Administrator on 2015/1/21.
+ */
+public class TestClone {
+
+    public static void main(String[] args) {
+
+        SerializationUtils.clone(null);
+    }
+}
